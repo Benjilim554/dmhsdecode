208213b0868df8a9b4109633ba8ab2e37a0bf6cf
Benjilim554 <benjilim2009@gmail.com>
2026-01-20 23:00:51 -0800

    Updated implementations and PID
    +Updated implementations for panels, PPTelem, PP, etc..
    +Added Panels support & Panels telemetry

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FlywheelTuning.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FlywheelTuning.java
@@ -1,6 +1,9 @@
 package org.firstinspires.ftc.teamcode;
 
 
+import com.bylazar.telemetry.PanelsTelemetry;
+import com.bylazar.telemetry.JoinedTelemetry;
+
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -10,6 +13,21 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import org.firstinspires.ftc.teamcode.subsystem.util.PIDFController;
 
 import com.bylazar.configurables.annotations.Configurable;
+
+/*
+
+1) kS stands for "static." It's the greatest power you can give the flywheel motor(s) without it moving.
+So it should be a really small number below like 0.05 or something.
+You should increase it until it begins to move and then decrease it until it stops moving again.
+2) kV is the next number you tune. Pick a velocity like 1500 or something and then increase kV until it
+gets there.
+3) Tune P. Send a ball through the launcher and increase P until it doesn't overshoot past the target
+velocity in the recovery phase. Keep increasing though because unless you see overshoot you're fine.
+4) Tune I. After all the other numbers are tuned, save them, and then make kS slightly negative.
+This will simulate a drop in power to the motor from a low battery. Increase I until your confident
+that you can still get to your target velocity.
+
+ */
 
 @Configurable
 @TeleOp
@@ -21,6 +39,7 @@ public class FlywheelTuning extends OpMode {
     @Override
     public void init() {
         //TODO: Set motor name and direction
+        telemetry = new JoinedTelemetry(PanelsTelemetry.INSTANCE.getFtcTelemetry(), telemetry);
         motor = (DcMotorEx) hardwareMap.get(DcMotor.class, "motorName");
         motor.setDirection(DcMotorSimple.Direction.FORWARD);
         controller = new PIDFController(P, I, 0.0, 0.0);
